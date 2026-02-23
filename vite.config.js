36cf5057a080e78a90ddaa985b385dd031fc366e
Umeshbandari <umeshbandari19@gmail.com>
2026-02-23 20:09:38 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-   base: '/FarmTracker/',
+   base: "/Farm",
    plugins: [
     react(),
     tailwindcss(),
